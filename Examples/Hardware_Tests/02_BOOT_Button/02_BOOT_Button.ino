1b2144219e19fc877a49ad41173489afb4d12507
Paul <pc235603@gmail.com>
2022-10-27 10:57:03 +0100

    Update 02_BOOT_Button.ino

--- Examples/Hardware_Tests/02_BOOT_Button/02_BOOT_Button.ino
+++ Examples/Hardware_Tests/02_BOOT_Button/02_BOOT_Button.ino
@@ -1,5 +1,5 @@
 // OpenLog ESP32 Test Example
-// Tested with Espressif ESP32 v2.0.4 and the "ESP32 Dev Module" board definition
+// Tested with Espressif ESP32 v2.0.5 and the "ESP32 Dev Module" board definition
 
 #define BOOT_BUTTON 0 // OpenLog ESP32 Boot Button is connected to D0
 #define STAT_LED 25 // OpenLog ESP32 STAT LED is connected to D25
