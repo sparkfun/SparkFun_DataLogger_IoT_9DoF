90d055e4577d8aee380f062cac27d5df93c6b8f5
Paul <pc235603@gmail.com>
2022-10-27 11:26:39 +0100

    Update 06_Qwiic_u-blox.ino

--- Examples/Hardware_Tests/06_Qwiic_u-blox/06_Qwiic_u-blox.ino
+++ Examples/Hardware_Tests/06_Qwiic_u-blox/06_Qwiic_u-blox.ino
@@ -1,5 +1,5 @@
 // OpenLog ESP32 Test Example
-// Tested with Espressif ESP32 v2.0.4 and the "ESP32 Dev Module" board definition
+// Tested with Espressif ESP32 v2.0.5 and the "ESP32 Dev Module" board definition
 
 #define EN_3V3_SW 32 // The 3.3V_SW regulator Enable pin is connected to D32
 
